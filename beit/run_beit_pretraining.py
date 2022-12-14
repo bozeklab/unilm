174a91c3ef7eec9a70615045a02c7ec33ebe6fc4
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2022-12-14 13:52:02 +0100

    CIFAR dataset

--- beit/run_beit_pretraining.py
+++ beit/run_beit_pretraining.py
@@ -129,6 +129,9 @@ def get_args():
     parser.add_argument('--dist_on_itp', action='store_true')
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
 
+    parser.add_argument('--data_set', default='IMNET', choices=['CIFAR', 'IMNET', 'image_folder'],
+                        type=str, help='ImageNet dataset path')
+
     return parser.parse_args()
 
 
