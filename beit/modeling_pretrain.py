bb591878f6a4d020190d9b10c79a6a225cc5386f
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2022-12-14 12:38:32 +0100

    Added pretrained model

--- beit/modeling_pretrain.py
+++ beit/modeling_pretrain.py
@@ -189,3 +189,12 @@ def beit_large_patch16_224_8k_vocab(pretrained=False, **kwargs):
         )
         model.load_state_dict(checkpoint["model"])
     return model
+
+
+@register_model
+def beit_large_patch16_384(pretrained=False, **kwargs):
+    model = VisionTransformerForMaskedImageModeling(
+        img_size=384, patch_size=16, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    return model
