14633568696a61b87a709e2b5a7c58b47519557e
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-04 20:50:32 +0100

    convert boxes

--- beit/segmentation_utils/convert_to_bboxes.py
+++ beit/segmentation_utils/convert_to_bboxes.py
@@ -70,8 +70,10 @@ def create_bboxes_for_mask(masks):
     bboxes = []
     for prop in props:
         bbox = prop.bbox
+        area = (bbox[3] - bbox[0]) * (bbox[4] - bbox[1])
+        if area > 20:
+            continue
         bboxes.append(torch.tensor([bbox[1], bbox[0], bbox[4], bbox[3]]).unsqueeze(0))
-        print((bbox[3] - bbox[0]) * (bbox[4] - bbox[1]))
     return torch.cat(bboxes, dim=0)
 
 
