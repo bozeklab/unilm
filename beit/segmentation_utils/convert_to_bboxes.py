b1620ff20d5609dc6dca1a70494375f0307549fd
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-04 19:30:52 +0100

    convert boxes

--- beit/segmentation_utils/convert_to_bboxes.py
+++ beit/segmentation_utils/convert_to_bboxes.py
@@ -7,7 +7,7 @@ import pickle
 from os.path import isfile, join
 
 import torchvision.transforms.functional as F
-from torchvision.ops import masks_to_boxes, nms, box_convert
+#from torchvision.ops import masks_to_boxes, nms, box_convert
 from tqdm import tqdm
 
 from skimage.measure import label, regionprops
@@ -29,36 +29,36 @@ def show(imgs):
         axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 
 
-def expand_bounding_box(bbox, margin, img_shape):
-    x_fill = 2 * margin
-    x_fill -= min(margin, bbox[0])
-    xmin = max(bbox[0] - margin, 0)
-    xmax = min(bbox[2] + x_fill, img_shape[0] - 1)
-
-    y_fill = 2 * margin
-    y_fill -= min(margin, bbox[2])
-    ymin = max(bbox[1] - margin, 0)
-    ymax = min(bbox[3] + y_fill, img_shape[1] - 1)
-
-    if (0.0 in bbox) or (xmax >= img_shape[0] - 1) or (ymax >= img_shape[1] - 1):
-        return None
-
-    return box_convert(torch.tensor([xmin, ymin, xmax, ymax]), 'xyxy', 'xyxy').unsqueeze(0)
-
-
-def create_bboxes_for_image(masks, margin=10, img_shape=(448, 448)):
-    boxes = masks_to_boxes(masks)
-    boxes_u = nms(boxes, torch.ones(boxes.shape[0], dtype=torch.float), 0.50)
-    boxes_id = boxes[boxes_u]
-    boxes_expanded = []
-    for i in range(boxes_id.shape[0]):
-        if margin > 0:
-            expanded = expand_bounding_box(boxes_id[i].numpy(), margin=margin, img_shape=img_shape)
-            if expanded is not None:
-                boxes_expanded.append(expanded)
-        else:
-            boxes_expanded.append(boxes_id[i].unsqueeze(0))
-    return torch.cat(boxes_expanded, dim=0)
+# def expand_bounding_box(bbox, margin, img_shape):
+#     x_fill = 2 * margin
+#     x_fill -= min(margin, bbox[0])
+#     xmin = max(bbox[0] - margin, 0)
+#     xmax = min(bbox[2] + x_fill, img_shape[0] - 1)
+#
+#     y_fill = 2 * margin
+#     y_fill -= min(margin, bbox[2])
+#     ymin = max(bbox[1] - margin, 0)
+#     ymax = min(bbox[3] + y_fill, img_shape[1] - 1)
+#
+#     if (0.0 in bbox) or (xmax >= img_shape[0] - 1) or (ymax >= img_shape[1] - 1):
+#         return None
+#
+#     return box_convert(torch.tensor([xmin, ymin, xmax, ymax]), 'xyxy', 'xyxy').unsqueeze(0)
+#
+#
+# def create_bboxes_for_image(masks, margin=10, img_shape=(448, 448)):
+#     boxes = masks_to_boxes(masks)
+#     boxes_u = nms(boxes, torch.ones(boxes.shape[0], dtype=torch.float), 0.50)
+#     boxes_id = boxes[boxes_u]
+#     boxes_expanded = []
+#     for i in range(boxes_id.shape[0]):
+#         if margin > 0:
+#             expanded = expand_bounding_box(boxes_id[i].numpy(), margin=margin, img_shape=img_shape)
+#             if expanded is not None:
+#                 boxes_expanded.append(expanded)
+#         else:
+#             boxes_expanded.append(boxes_id[i].unsqueeze(0))
+#     return torch.cat(boxes_expanded, dim=0)
 
 
 def create_bboxes_for_mask(masks):
