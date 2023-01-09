504694f4a3c573cd713f4b4d14ab92563315b7dc
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-09 17:27:53 +0100

    Save pickle

--- beit/run_beit_inference.py
+++ beit/run_beit_inference.py
@@ -86,6 +86,7 @@ def infere(model, dataset, patch_size, device):
             batch_size, seq_len, C = x.shape
             x = x.view(batch_size, img.shape[2] // patch_size[0], img.shape[3] // patch_size[1], C)
         aligned_boxes = roi_align(input=x.permute(0, 3, 1, 2), boxes=[boxes], output_size=(3, 3))
+        print(aligned_boxes)
         m = nn.AvgPool2d(3, stride=1)
         aligned_boxes = m(aligned_boxes).squeeze()
 
