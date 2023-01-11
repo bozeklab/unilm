8b5270c4368c35c741769568289d469c9c2c8bf3
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-11 20:31:02 +0100

    Data augmentation script

--- beit/segmentation_utils/augment_ds.py
+++ beit/segmentation_utils/augment_ds.py
@@ -5,8 +5,21 @@ import torchvision.transforms as T
 from torchvision.io import read_image
 import torch
 import glob
+import numpy as np
+import pickle
 
 IMG_DIR = '/data/pwojcik/images_he/'
+IMG_SIZE = 512
+
+
+def get_bounding_boxes(boxes, x_offset, y_offset):
+    idx = []
+    for i in range(boxes.shape[0]):
+        if (boxes[i, 0] >= y_offset) and (boxes[i, 1] >= x_offset) \
+                and (boxes[i, 2] < y_offset + IMG_SIZE) and (boxes[i, 3] < x_offset + IMG_SIZE):
+            idx.append(i)
+    return idx
+
 
 if __name__ == '__main__':
     files = glob.glob(os.path.join(IMG_DIR, '*'))
@@ -25,10 +38,15 @@ if __name__ == '__main__':
             ld = ic, jr + 1
             rd = ic + 1, jr + 1
             flu = f"wsi_001-tile-r{lu[0]}-c{lu[1]}.png"
+            flu_p = f"wsi_001-tile-r{lu[0]}-c{lu[1]}.pkl"
             fru = f"wsi_001-tile-r{ru[0]}-c{ru[1]}.png"
+            fru_p = f"wsi_001-tile-r{ru[0]}-c{ru[1]}.pkl"
             fld = f"wsi_001-tile-r{ld[0]}-c{ld[1]}.png"
+            fld_p = f"wsi_001-tile-r{ld[0]}-c{ld[1]}.pkl"
             frd = f"wsi_001-tile-r{rd[0]}-c{rd[1]}.png"
+            frd_p = f"wsi_001-tile-r{rd[0]}-c{rd[1]}.pkl"
             files = [flu, fru, fld, frd]
+            pickle_files = [flu_p, fru_p, fld_p, frd_p]
             all = True
             for f in files:
                 if not os.path.exists(os.path.join(IMG_DIR, f)):
@@ -36,22 +54,35 @@ if __name__ == '__main__':
                     break
             if not all:
                 continue
-            print(ic, jr)
+            pickles = [np.load(os.path.join(IMG_DIR, f), allow_pickle=True) for f in pickle_files]
+            pickles[1][0::2] += IMG_SIZE
+            pickles[2][1::2] += IMG_SIZE
+
+            pickles[3][0::2] += IMG_SIZE
+            pickles[3][1::2] += IMG_SIZE
+
             images = [read_image(os.path.join(IMG_DIR, f)) for f in files]
             image1 = torch.cat([images[0], images[1]], dim=1)
             image2 = torch.cat([images[2], images[3]], dim=1)
             image = torch.cat([image1, image2], dim=2)
             #image_boxes = T.ToPILImage()(image)
             #canvas = ImageDraw.Draw(image_boxes)
+            offsets = [(random.randint(10, IMG_SIZE), random.randint(10, IMG_SIZE)) for _ in range(10)]
             for cc in range(10):
-                a = random.randint(10, 512)
-                b = random.randint(10, 512)
-                crop = image[:, a:(a + 512), b:(b + 512)]
-                shape = [a, b, a + 512, b + 512]
+                a = random.randint(10, IMG_SIZE)
+                b = random.randint(10, IMG_SIZE)
+                idx = get_bounding_boxes(torch.cat(pickles), a, b)
+                pickles_n = [np.load(os.path.join(IMG_DIR, f), allow_pickle=True) for f in pickle_files]
+                pckls = [pickles_n[i] for i in pickles_n]
+
+                crop = image[:, a:(a + IMG_SIZE), b:(b + IMG_SIZE)]
+                shape = [a, b, a + IMG_SIZE, b + IMG_SIZE]
                 #canvas.rectangle(shape, outline="blue")
-                assert(crop.shape == (3, 512, 512))
+                assert(crop.shape == (3, IMG_SIZE, IMG_SIZE))
                 crop = T.ToPILImage()(crop)
-                crop.save(os.path.join(IMG_DIR, f"wsi_001-tile-r{lu[0]}-c{lu[1]}_{cc}aug.png"))
+                crop.save(os.path.join(IMG_DIR, f"wsi_001-tile-r{a}-c{b}aug.png"))
+                with open(os.path.join(IMG_DIR, f"wsi_001-tile-r{a}-c{b}aug.pkl"), 'wb') as outf:
+                    pickle.dump(torch.cat(pckls), outf)
                 #crop.show()
             #image_boxes.show()
 
