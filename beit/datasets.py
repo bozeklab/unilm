0bb82cfb3cc364ed302be97c749927f074262824
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-16 20:58:11 +0100

    Dataset masks

--- beit/datasets.py
+++ beit/datasets.py
@@ -48,7 +48,7 @@ class DataAugmentationForBEiT(object):
             interpolation=args.train_interpolation, second_interpolation=args.second_interpolation,
         )
 
-        self.random_hflip = RandomHorizontalFlip(p=1.0)
+        self.random_hflip = RandomHorizontalFlip(p=0.5)
 
         self.patch_transform = transforms.Compose([
             transforms.ToTensor(),
