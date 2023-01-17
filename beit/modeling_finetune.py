172c79d3764886153edb82b825609a71f995cc55
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-17 21:27:25 +0100

    Engine

--- beit/modeling_finetune.py
+++ beit/modeling_finetune.py
@@ -141,7 +141,7 @@ class Attention(nn.Module):
         if rel_pos_bias is not None:
             attn = attn + rel_pos_bias
         if attention_mask is not None:
-            attn = attn.masked_fill(attention_mask == False, torch.finfo(attn.dtype).min)
+            attn = attn.masked_fill(attention_mask == True, torch.finfo(attn.dtype).min)
         attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
 
