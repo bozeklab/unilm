5c075ba8a51c16049151d613e71f804a5dfcb5d6
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-17 22:03:45 +0100

    Engine

--- beit/datasets.py
+++ beit/datasets.py
@@ -127,6 +127,7 @@ class DataAugmentationForBEiT(object):
             else:
                 crop = image[:, int(boxes[i, 1]): int(boxes[i, 3]), int(boxes[i, 0]): int(boxes[i, 2])]
                 crop = F.resize(crop, size=size)
+                print(crop)
             crops.append(crop.unsqueeze(dim=0))
         return torch.cat(crops, dim=0)
 
