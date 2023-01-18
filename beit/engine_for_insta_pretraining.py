ade2f62c4a5f19c648970617d4edfa8e5362e301
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-18 18:46:04 +0100

    Engine

--- beit/engine_for_insta_pretraining.py
+++ beit/engine_for_insta_pretraining.py
@@ -62,6 +62,10 @@ def train_one_epoch(model: torch.nn.Module, d_vae: torch.nn.Module,
             img_outputs, insta_outputs = model(samples, boxes, bool_masked_pos=bool_masked_pos, attention_mask=attention_mask,
                                 boxes_mask=masked_boxes, return_all_tokens=False)
             img_loss = nn.CrossEntropyLoss()(input=img_outputs, target=labels)
+            print(img_outputs.shape)
+            print(labels.shape)
+            print(insta_outputs.shape)
+            print(instance_labels.shape)
             insta_loss = nn.CrossEntropyLoss()(input=insta_outputs, target=instance_labels)
             total_loss = img_loss + insta_loss
 
