61a50c806deab71acfefb099667c70fb8fa39378
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-24 21:42:26 +0100

    Added finetune model

--- beit/run_beit_inference.py
+++ beit/run_beit_inference.py
@@ -114,7 +114,8 @@ def infere(model, dataset, device):
 
         for i in range(aligned_boxes.shape[0]):
             embeddings.append(aligned_boxes[i].numpy())
-            label = classes[0][i]
+            #label = classes[0][i]
+            label = classes[i]
             labels.append(label)
             box = boxes[i].numpy().tolist()
             crop = nonnormalized_img[:, int(box[1]):int(box[3]), int(box[0]):int(box[2])]
