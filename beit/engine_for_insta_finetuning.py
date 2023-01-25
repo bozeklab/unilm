8e2fe2a8202a9dfc661ba860061a3db9d2c84f9b
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-25 22:33:47 +0100

    Added finetune model

--- beit/engine_for_insta_finetuning.py
+++ beit/engine_for_insta_finetuning.py
@@ -186,10 +186,6 @@ def evaluate_f1_whole(args, model, device):
         _predictions[_pred_idx] = 1
         _predictions[~_pred_idx] = 0
 
-        print(i)
-        print(_predictions)
-        print(_labels)
-
 
         print(f"{types[i]} class F1 {f1_score(_labels.numpy(), _predictions.numpy())}")
     print()
