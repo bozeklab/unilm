8786902423db1e47ceff51b6e33ff07496423665
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-25 18:16:35 +0100

    Added finetune model

--- beit/run_class_insta_finetuning.py
+++ beit/run_class_insta_finetuning.py
@@ -303,7 +303,6 @@ def main(args, ds_init):
         use_mean_pooling=args.use_mean_pooling,
         init_scale=args.init_scale,
         use_rel_pos_bias=args.rel_pos_bias,
-        use_abs_pos_emb=args.abs_pos_emb,
         init_values=args.layer_scale_init_value,
     )
 
