2550c80d40d316213b448c724f8b809e2f70dfcd
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-27 16:41:30 +0100

    Insta finetune from pt

--- beit/modeling_insta_finetune_from_pt.py
+++ beit/modeling_insta_finetune_from_pt.py
@@ -103,7 +103,7 @@ class VisionTransformerFromPretrained(nn.Module):
     def extract_alligned_rois(self, x, boxes):
             batch_size, seq_len, C = x.shape
             x = x.view(batch_size, self.img_size // self.patch_size, self.img_size // self.patch_size, C)
-            aligned_boxes = roi_align(input=x.permute(0, 3, 1, 2), spatial_scale=1 / self.patch_size[0],
+            aligned_boxes = roi_align(input=x.permute(0, 3, 1, 2), spatial_scale=1 / self.patch_size,
                                       boxes=[boxes], output_size=(3, 3))
             m = nn.AvgPool2d(3, stride=1)
             return m(aligned_boxes)
