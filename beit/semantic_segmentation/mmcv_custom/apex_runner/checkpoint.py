c20854923ca8f2ba6633473a7d285a7ad06cc6f2
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-29 18:23:53 +0100

    Insta finetune from pt

--- beit/semantic_segmentation/mmcv_custom/apex_runner/checkpoint.py
+++ beit/semantic_segmentation/mmcv_custom/apex_runner/checkpoint.py
@@ -10,10 +10,10 @@ import mmcv
 from mmcv.parallel import is_module_wrapper
 from mmcv.runner.checkpoint import weights_to_cpu, get_state_dict
 
-try:
-    import apex
-except:
-    print('apex is not installed')
+#try:
+#    import apex
+#except:
+#    print('apex is not installed')
 
 
 def save_checkpoint(model, filename, optimizer=None, meta=None):
@@ -55,7 +55,7 @@ def save_checkpoint(model, filename, optimizer=None, meta=None):
             checkpoint['optimizer'][name] = optim.state_dict()
 
     # save amp state dict in the checkpoint
-    checkpoint['amp'] = apex.amp.state_dict()
+    #checkpoint['amp'] = apex.amp.state_dict()
 
     if filename.startswith('pavi://'):
         try:
