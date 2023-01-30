8b17e0d05b7b3727b01a6afd124bd158c3d9bdc3
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-01-30 13:38:41 +0100

    Insta finetune from pt

--- beit/datasets.py
+++ beit/datasets.py
@@ -225,8 +225,8 @@ class DataAugmentationForBEITDataset(object):
                         (fake_box.expand(self.num_boxes, -1), fake_class.expand(self.num_boxes, -1))]
             else:
                 boxes, classes = boxes
-                #classes = DataAugmentationForBEITDataset._merge_classes(classes)
-                classes = classes.type(torch.int64)
+                classes = DataAugmentationForBEITDataset._merge_classes(classes)
+                #classes = classes.type(torch.int64)
                 boxes_available = boxes.shape[0]
                 if boxes.shape[0] <= self.num_boxes:
                     padding_length = self.num_boxes - boxes_available
@@ -249,8 +249,8 @@ class DataAugmentationForBEITDataset(object):
                     boxes]
         else:
             boxes, classes = boxes
-            #classes = DataAugmentationForBEITDataset._merge_classes(classes)
-            classes = classes.type(torch.int64)
+            classes = DataAugmentationForBEITDataset._merge_classes(classes)
+            #classes = classes.type(torch.int64)
             return [self.patch_transform(image),
                     transforms.ToTensor()(image),
                     (boxes, classes)]
